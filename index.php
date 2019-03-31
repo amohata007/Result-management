f79efd7ec02a0cf2e422913f6524d2fbeaf2d741
Abhishek mohata <36244104+amohata007@users.noreply.github.com>
2019-03-31 10:18:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ echo "<script type='text/javascript'> document.location = 'dashboard.php'; </scr
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="utf-8">
+        <meta charset="utf-8">  
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
     	<meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Admin Login</title>
